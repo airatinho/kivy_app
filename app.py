ea08c65061ebd48f01439d0259bba43833d3152f
karim <123>
2022-01-25 16:42:08 +0300

    ready

--- app.py
+++ app.py
@@ -3,14 +3,37 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+from kivy.config import Config
+
+Config.set('kivy','keyboard_mode','systemanddoc')
+Window.size=(480,853)
+
+
+def get_ing(mas):
+    nitr=str(10*mas/1000)
+    sald=str(15*mas/1000)
+    start=str(0.5*mas/1000)
+    sugars=str(5*mas/1000)
+    time_v=str(round(mas / 500*2))
+    return {"nitro":nitr,"sald":sald,"start":start,"sugars":sugars,"time_v":time_v}
 
 
 class Container(GridLayout):
-    label_widget = ObjectProperty()
-    text_input = ObjectProperty()
-    
-    def change_text(self):
-        self.label_widget.text = self.text_input.text.upper()
+
+    def calculate(self):
+        try:
+            mas = int(self.text_input.text)
+        except Exception:
+            mas= 0
+
+        ingredients = get_ing(mas)
+        self.sald.text=ingredients['sald']+ ' +5'
+        self.nitr.text=ingredients['nitro']
+        self.sugars.text = ingredients['sugars']
+        self.start.text = ingredients['start']
+        self.time_v.text = ingredients['time_v']
+
 class MyApp(App):
     def build(self):
 
